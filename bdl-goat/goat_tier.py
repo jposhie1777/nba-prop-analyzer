90f5bb62115630fab68af4a10b28bbd71f673be5
jposhie1777 <jposhie1777@gmail.com>
2025-12-24 22:34:35 -0500

    Update goat_tier.py

--- bdl-goat/goat_tier.py
+++ bdl-goat/goat_tier.py
@@ -569,5 +569,4 @@ def ingest_all():
 # MAIN
 # ======================================================
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", "8080"))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8080, debug=True)

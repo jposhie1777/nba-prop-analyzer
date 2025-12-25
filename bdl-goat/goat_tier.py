4ae87e9e9fb7092d3a4fb441b2c3fd8ad15f066c
jposhie1777 <jposhie1777@gmail.com>
2025-12-25 13:55:16 -0500

    Update goat_tier.py

--- bdl-goat/goat_tier.py
+++ bdl-goat/goat_tier.py
@@ -280,4 +280,4 @@ def route_props():
 # MAIN
 # ======================================================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)

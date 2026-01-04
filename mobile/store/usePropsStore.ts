2994100c2473036de078252b7799feb091a6cee5
jposhie1777 <jposhie1777@gmail.com>
2026-01-03 23:14:21 -0500

    Create usePropsStore.ts

---
+++ mobile/store/usePropsStore.ts
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+import { MobileProp } from "@/lib/api";
+
+type PropsStore = {
+  props: MobileProp[];
+  setProps: (props: MobileProp[]) => void;
+};
+
+export const usePropsStore = create<PropsStore>((set) => ({
+  props: [],
+  setProps: (props) => set({ props }),
+}));

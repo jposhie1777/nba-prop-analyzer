c8c015fbdf24ec2901aa693ecf9e1aa44ed96e94
jposhie1777 <jposhie1777@gmail.com>
2026-01-04 22:49:14 -0500

    Update index.ts

--- mobile/theme/index.ts
+++ mobile/theme/index.ts
@@ -1,25 +1,14 @@
 // theme/index.ts
-
 import base from "./color";
 import apple from "./colors.apple";
 import fanduel from "./colors.fanduel";
 import fanduelLight from "./colors.fanduelLight";
 
-import type { ThemeColors } from "./types";
-
-/* ======================================================
-   THEMES MAP
-====================================================== */
-
-export const themes: Record<string, ThemeColors> = {
+export const themes = {
   base,
   apple,
   fanduel,
   fanduelLight,
 };
 
-/* ======================================================
-   DEFAULT EXPORT
-====================================================== */
-
-export default themes;
+export type ThemeKey = keyof typeof themes;

a3301d0ec8cdaf006d0b8ed5908fcfd1d10ce4c8
jposhie1777 <jposhie1777@gmail.com>
2026-01-04 21:36:26 -0500

    Create useTheme.ts

---
+++ mobile/store/useTheme.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+import { themes, ThemeName } from "@/theme";
+
+type ThemeState = {
+  theme: ThemeName;
+  colors: typeof themes.base;
+  setTheme: (theme: ThemeName) => void;
+};
+
+export const useTheme = create<ThemeState>((set) => ({
+  theme: "base",
+  colors: themes.base,
+
+  setTheme: (theme) =>
+    set({
+      theme,
+      colors: themes[theme],
+    }),
+}));

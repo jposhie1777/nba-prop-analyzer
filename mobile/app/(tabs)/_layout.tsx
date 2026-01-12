96e5dfdd4b463eb8b8f149baf402275aa4565056
jposhie1777 <jposhie1777@gmail.com>
2026-01-12 18:38:43 -0500

    Update _layout.tsx

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-//   /app/(abs)/_layout.tsx
+//   /app/(tabs)/_layout.tsx
 import { Tabs } from "expo-router";
 import React, { useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";

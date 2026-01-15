f0061e6ecb0735edf87b9c8f0d1752f0ca83013d
jposhie1777 <jposhie1777@gmail.com>
2026-01-15 10:30:44 -0500

    Update logging.py

--- mobile_api/ingest/common/logging.py
+++ mobile_api/ingest/common/logging.py
@@ -1,4 +1,5 @@
+# mobile_api/ingest/common/logging.py
 from datetime import datetime, timezone
 
-def now_ts():
-    return datetime.now(timezone.utc)
+def now_ts() -> str:
+    return datetime.now(timezone.utc).isoformat()

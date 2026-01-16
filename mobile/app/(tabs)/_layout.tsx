a619d18fec8f2a73c2d41bfebad319572b7fbc73
jposhie1777 <jposhie1777@gmail.com>
2026-01-16 13:18:43 -0500

    Update _layout.tsx

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -6,7 +6,7 @@ import { HapticTab } from "@/components/haptic-tab";
 import { IconSymbol } from "@/components/ui/icon-symbol";
 import { Colors } from "@/constants/theme";
 import { useColorScheme } from "@/hooks/use-color-scheme";
-
+import { Ionicons } from "@expo/vector-icons";
 
 const SAVED_PROPS_KEY = "saved_props_v1";
 

f17e898dd3faf8d302f700babb161d3a7a440858
jposhie1777 <jposhie1777@gmail.com>
2026-01-18 22:22:26 -0500

    Create stats.ts

---
+++ mobile/lib/stats.ts
@@ -0,0 +1,7 @@
+// lib/stats.ts
+export const STAT_META = {
+  pts: { label: "PTS", unit: "pts" },
+  reb: { label: "REB", unit: "reb" },
+  ast: { label: "AST", unit: "ast" },
+  "3pm": { label: "3PM", unit: "threes" },
+} as const;

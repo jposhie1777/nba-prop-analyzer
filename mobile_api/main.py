267c4a7f2b57aa12d0c9233b79bbf6d4906a82a7
jposhie1777 <jposhie1777@gmail.com>
2026-01-22 20:03:01 +0000

    ch

--- mobile_api/main.py
+++ mobile_api/main.py
@@ -25,8 +25,6 @@ from routes.first_basket import router as first_basket_router
 from routes.teams import router as teams_router
 from routes.prop_analytics import router as prop_analytics_router
 from routes.players_routes import router as players_router
-from routes.player_props_master import router as master_props_router
-from routes.ingest_player_props_master import router as ingest_master_router
 from routes.ingest import router as ingest_router
 
 
@@ -105,8 +103,6 @@ app.include_router(player_box_router)
 app.include_router(player_stats_router)
 app.include_router(prop_analytics_router)
 app.include_router(players_router)
-app.include_router(master_props_router)
-app.include_router(ingest_master_router)
 app.include_router(ingest_router)
 
 

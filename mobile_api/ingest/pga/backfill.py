6f4d7d6d9f1171b740ade35862e26a4b27f9af79
jposhie1777 <jposhie1777@gmail.com>
2026-02-03 13:57:40 +0000

    CH

--- mobile_api/ingest/pga/backfill.py
+++ mobile_api/ingest/pga/backfill.py
@@ -10,7 +10,7 @@ from typing import Any, Dict, Iterable, List, Optional
 from google.cloud import bigquery
 
 # Allow running as a script: python mobile_api/ingest/pga/backfill.py
-REPO_ROOT = Path(__file__).resolve().parents[4]
+REPO_ROOT = Path(__file__).resolve().parents[3]
 sys.path.insert(0, str(REPO_ROOT))
 
 from mobile_api.pga.client import PgaApiError, fetch_paginated
